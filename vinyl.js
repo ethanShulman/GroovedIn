24207e56ac1f0e7c96223d901f6dd7ecc2d92438
COR Coding <corcoding@android-3755cdb9ae9c6cf1.localdomain>
2023-08-15 11:16:46 -0400

    added vinyl html add vinyl javascript.

---
+++ vinyl.js
@@ -0,0 +1,36 @@
+const recordBtn = document.getElementById('record-btn');
+let trackBtnClick=-1
+const dataDiv= document.getElementById('data-container');
+const url = 'https://spotify81.p.rapidapi.com/top_20_by_monthly_listeners';
+    const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '32c60e1b81mshf734d0f60eb527cp1a7e82jsnd97cef222b63',
+        'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
+    }
+};
+async function songFetch(){
+try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log("result", result)
+    result.forEach(song=>{
+        console.log("song,",song)
+        console.log(song.artist.monthlyListeners)
+        let artistName=document.createElement('p')
+        let monthlyPeeps=document.createElement('p')
+        artistName.innerText=song.artist
+        monthlyPeeps.innerText=song.monthlyListeners
+        dataDiv.appendChild(artistName)
+        dataDiv.appendChild(monthlyPeeps)
+        result[trackBtnClick].song.artist.monthlyListeners
+    })
+} catch (error) {
+    console.error(error);
+}}
+recordBtn.addEventListener('click',()=>{
+    trackBtnClick+=1
+    console.log(trackBtnClick);
+    songFetch();
+})
+// result[trackBtnClick].song.artist.monthlyListener
